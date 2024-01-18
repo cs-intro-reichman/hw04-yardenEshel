6cdb90dcb03ae9b4aa5f8a04d3e8145cc33db429
Yarden Eshel <yardenyarden5@gmail.com>
2024-01-18 21:05:51 +0200

    all

--- StringOps.java
+++ StringOps.java
@@ -42,6 +42,10 @@ public class StringOps {
                     newString+=string.charAt(i);
                 }
             }
+            else
+            {
+                newString += " ";
+            }
             
             
         }
